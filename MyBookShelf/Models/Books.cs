880190954b58084eee7534e22461c3adb6074629
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-02-17 15:12:13 +0200

    feat: add Book model

---
+++ MyBookShelf/Models/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookShelf.Models
+{
+    public class Book
+    {
+        public int IdBook { get; set; }
+        public string Title { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string PathImg { get; set; }
+        public int IdShelf { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int CountPages { get; set; }
+ 
+    }
+}
