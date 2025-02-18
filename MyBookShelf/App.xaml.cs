e238bd7f4ab33c694a5fab80dbc41ac7fc4e5bae
Andriy Sozanskiy <sozanskiyandriyj@gmail.com>
2025-02-18 16:33:38 +0200

    feat: added repositories for Book, BookGenre, and Genre

--- MyBookShelf/App.xaml.cs
+++ MyBookShelf/App.xaml.cs
@@ -1,17 +1,24 @@
 ﻿using System.Windows;
 using MyBookShelf.DBContext;
 using Microsoft.EntityFrameworkCore;
-
+using MyBookShelf.Repositories.BookRroviders;
+using MyBookShelf.Repositories.GenreRroviders;
+using MyBookShelf.Repositories.BookGenreRroviders;
 namespace MyBookShelf
 {
     public partial class App : Application
     {
         private const string CONNECTION_ST = "connection";
         private readonly BookShelfDbContextFactory _bookShelfDbContextFactory;
-
+        private readonly IBookProviders _bookProviders;
+        private readonly IGenreProviders _genreProviders;
+        private readonly IBookGenreProviders _bookGenreProviders;
         public App()
         {
             _bookShelfDbContextFactory = new BookShelfDbContextFactory(CONNECTION_ST);
+            _bookProviders = new DatabaseBookProviders(_bookShelfDbContextFactory);
+            _genreProviders = new DatabaseGenreProviders(_bookShelfDbContextFactory);
+            _bookGenreProviders = new DatabaseBookGenreProviders(_bookShelfDbContextFactory);
         }
 
         protected async override void OnStartup(StartupEventArgs e)
@@ -22,6 +29,7 @@ namespace MyBookShelf
                 dbContext.Database.Migrate();
 
             }
+
             MainWindow = new MainWindow();
             MainWindow.Show();
             base.OnStartup(e);
---
+++ MyBookShelf/Repositories/BookRroviders/IBookProviders.cs
@@ -0,0 +1,8 @@
+﻿using MyBookShelf.Models;
+
+namespace MyBookShelf.Repositories.BookRroviders
+{
+    public interface IBookProviders:IRepository<Book>
+    {
+    }
+}
