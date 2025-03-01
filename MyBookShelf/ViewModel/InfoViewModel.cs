478808fa01c928b241a57dba1cee209645c5afaf
ClouDF0X <savcukd11@gmail.com>
2025-03-01 20:19:08 +0200

    refactor: refactored Info

--- MyBookShelf/ViewModel/InfoViewModel.cs
+++ MyBookShelf/ViewModel/InfoViewModel.cs
@@ -11,6 +11,7 @@ namespace MyBookShelf.ViewModel
 {
     public class InfoViewModel : ViewModelBase
     {
+        // GIF paths for different tutorials
         private string _gifCreateShelf;
         public string GifCreateShelf
         {
@@ -46,6 +47,7 @@ namespace MyBookShelf.ViewModel
             set { _gifNotes = value; OnPropertyChanged(); }
         }
 
+        // Indicates if the GIFs are loading
         private bool _isGifLoading;
         public bool IsGifLoading
         {
@@ -53,29 +55,33 @@ namespace MyBookShelf.ViewModel
             set { _isGifLoading = value; OnPropertyChanged(); }
         }
 
+        // Constructor initializes GIF loading
         public InfoViewModel()
         {
             LoadGifAsync();
         }
 
+        // Asynchronously loads GIF paths
         private void LoadGifAsync()
         {
             IsGifLoading = true; 
             Task.Run(() =>
             {
+                // Define the file paths for the tutorial GIFs
                 string gifPath_shelf = "pack://application:,,,/Images/Info/tutorial_create_shelf.gif";
                 string gifPath_add_book = "pack://application:,,,/Images/Info/tutorial_add_book.gif";
                 string gifPath_reading_session = "pack://application:,,,/Images/Info/tutorial_reading_session.gif";
                 string gifPath_edit_book = "pack://application:,,,/Images/Info/tutorial_edit_book.gif";
                 string gifPath_notes = "pack://application:,,,/Images/Info/tutorial_notes.gif";
 
+                // Convert paths to URIs and assign them to properties
                 GifCreateShelf = new Uri(gifPath_shelf, UriKind.RelativeOrAbsolute).ToString();
                 GifAddBook = new Uri(gifPath_add_book, UriKind.RelativeOrAbsolute).ToString();
                 GifReadingSession = new Uri(gifPath_reading_session, UriKind.RelativeOrAbsolute).ToString();
                 GifEditBook = new Uri(gifPath_edit_book, UriKind.RelativeOrAbsolute).ToString();
                 GifNotes = new Uri(gifPath_notes, UriKind.RelativeOrAbsolute).ToString();
-                IsGifLoading = false;
                 
+                IsGifLoading = false;
             });
         }
     }
